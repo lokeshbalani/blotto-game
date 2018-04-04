601265e480932b661267aff652848e8099cd3a09
Lokesh Balani -X (lobalani - VIRTUSA CORPORATION at Cisco) <lobalani@partnet.cisco.com>
2018-04-04 22:44:27 +0530

    Blotto.py : Added methods to create a dataset with valid strategies randomly

--- Blotto.py
+++ Blotto.py
@@ -1,13 +1,95 @@
+# Authors: Lokesh Balani, Aditya Saripalli, 2018
+#
+# A simultor for a Weighted Colonel Blotto game that is played as follows:
+#
+# We are given battlefields labelled 1-n, and we say that the battlefield's
+# label is its value. Two players present attack strategies given
+# a fixed number of soldiers, deciding how many soldiers they want
+# to send to every battlefield. Once the bets are compared, the player with
+# the most soldiers at a battlefield wins that battlefield's points. For 4
+# battlefield and 10 soldiers, this might look like:
+#
+#
+#  Player 1's bets:   4   2   3   1
+#  ----------------------------------
+#  Battlefields:      1   2   3   4
+#  ----------------------------------
+#  Player 2's bets:   0   0   5   5
+#
+#
+# In this case, Player 1 is assigned a score of 3, because the player
+# won the battlefields labelled 1 and 2. Player 2 scores 7 for winning the
+# other two battlefields and thus wins the game
+#
+# 
+
 import random
 import os
 
 class Blotto:
     def __init__(self, n_sol, n_battles):
         '''Set the number of soldiers available and the number of battlefields'''
-        self.n_soldiers = n_sol
-        self.n_battlefields = n_battles
+        self.n_soldiers = int(n_sol)
+        self.n_battlefields = int(n_battles)
+        self.train_data = None
+        self.dataset = self.random_strategy_set(1000)
         print("#Soldiers : {}".format(self.n_soldiers))
         print("#Battlefields : {}".format(self.n_battlefields))
+
+        print("--------------------------------")
+        print("Dataset")
+        print("--------------------------------")
+        for i in range(len(self.dataset)):
+            print("Strategy #{} : {}".format(i + 1, self.dataset[i]))
+
+
+    def prepare_dataset(self):
+        return "Preparing Dataset"
+
+    # Reads in the given file and creates a list of betting selections from the data
+    def read_dataset(self, dataset_path):
+        return "dataset"
+
+    # Create a single random integer-valued strategy that sums to 
+    # No of soldiers with length equal to number of battlefields
+    def random_strategy(self):
+        # Create a random resource variable
+        res = self.n_soldiers
+
+        #Empty list
+        strategy = []
+
+        for i in range(self.n_battlefields - 1):
+            # Fill each position in strategy with a random number 
+            # between 0 and remaining reserves
+            rand = random.randint(0, res)
+            # add this to the strategy
+            strategy.append(rand)
+            # Reduce reserves by the allocated number
+            res -= rand
+
+        # Add any remaining reserves at the last
+        strategy.append(res)
+
+        # Shuffle the strategy
+        random.shuffle(strategy)
+
+        return strategy
+
+    def random_strategy_set(self, n_strategies):
+        #Empty list
+        strategy_set = []
+        strategy = []
+
+        #Keep going until we put n_strategies in strategy set
+        while len(strategy_set) < n_strategies:
+            strategy = self.random_strategy()
+
+            if sum(strategy)  == self.n_soldiers:
+                strategy_set.append(strategy)
+
+        return strategy_set
+
 
 
 print("Input the Number of Soldiers")
