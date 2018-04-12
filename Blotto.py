51a702966ff89d91e7beff741a25cc7218475651
Lokesh Balani -X (lobalani - VIRTUSA CORPORATION at Cisco) <lobalani@partnet.cisco.com>
2018-04-12 15:43:10 +0530

    Created Bot

--- Blotto.py
+++ Blotto.py
@@ -35,22 +35,22 @@ class Blotto:
         self.n_soldiers = int(n_sol)
         self.n_battlefields = int(n_bfs)
         self.train_data = None
-        self.r_dataset = self.random_strategy_set(1000)
+        self.r_dataset = self.random_strategy_set(5000)
         self.dataset = self.prepare_dataset(["./dataset/bf_strategies_1.csv", "./dataset/bf_strategies_2.csv"])
         print("#Soldiers : {}".format(self.n_soldiers))
         print("#Battlefields : {}".format(self.n_battlefields))
 
-        print("--------------------------------")
-        print("Random Generated Dataset")
-        print("--------------------------------")
-        for i in range(len(self.r_dataset)):
-            print("Strategy #{} : {}".format(i + 1, self.r_dataset[i]))
+        #print("--------------------------------")
+        #print("Random Generated Dataset")
+        #print("--------------------------------")
+        #for i in range(len(self.r_dataset)):
+            #print("Strategy #{} : {}".format(i + 1, self.r_dataset[i]))
 
-        print("--------------------------------")
-        print("Five Thirty Eight Dataset")
-        print("--------------------------------")
-        for i in range(len(self.dataset)):
-            print("Strategy #{} : {}".format(i + 1, self.dataset[i]))
+        #print("--------------------------------")
+        #print("Five Thirty Eight Dataset")
+        #print("--------------------------------")
+        #for i in range(len(self.dataset)):
+            #print("Strategy #{} : {}".format(i + 1, self.dataset[i]))
 
 
     def prepare_dataset(self,
@@ -165,10 +165,131 @@ class Blotto:
         #   number : score for the strategy
         return 3 * strategy_stats[0] + 2 * strategy_stats[1]
 
+    def duplicate_strat(self, strat):
+        cp_strat = []
+
+        for s in strat:
+            cp_strat.append(s)
+
+        return cp_strat
+
+    def mutation(self, strat):
+        # Mutates given selection slightly, 
+        # by decrementing one bet and incrementing
+        # another up to 10 times, selecred randomly
+
+        deploy_strat = self.duplicate_strat(strat)
+
+        # Pick a a number in [1,..,n_bfs] for number of mutations
+        n_mutations = random.randrange(self.n_battlefields)
+
+        for i in range(n_mutations - 1):
+            # Decrement deployment in one battlefield 
+            # and increment another 
+            # (these can be the same battlefields)
+
+            rand_idx_1 = random.randrange(self.n_battlefields)
+            rand_idx_2 = random.randrange(self.n_battlefields)
+
+            bfs_1 = deploy_strat[rand_idx_1]
+
+            if bfs_1 > 0:
+                deploy_strat[rand_idx_1] -= 1
+                deploy_strat[rand_idx_2] += 1
+
+        return deploy_strat
+
+class Bot:
+    def __init__(self, blotto_game):
+        self.game = blotto_game
+
+        # Learning Strategy Space size
+        self.n_players = 60
+        
+        # Initialise strategies for players randomly
+        self.player_strats = blotto_game.random_strategy_set(self.n_players)
+
+        print("Initial Player Strategies")
+        for i in range(len(self.player_strats)):
+            print("Strategy #{} : {}".format(i + 1, self.player_strats[i]))
+
+    def calc_pl_score(self, pl_strat):
+        return self.game.calc_final_score(self.game.pl_strategy_stats(pl_strat, self.game.r_dataset))
+
+    def pl_strat_sorted(self):
+        pl_scores = [(pl_strat, self.calc_pl_score(pl_strat)) for pl_strat in self.player_strats]
+
+        return sorted(pl_scores, key= lambda pl_score: pl_score[1])
+
+    def pl_strat_update(self):
+        # Creates the next generation of strategies 
+        # from the current one
+
+        # Maintains a list of selections, ranked by their scores. 
+        # Every time this list is updated, the top third of the list is maintained, 
+        # another third is composed of mutants of the top third, 
+        # and the final third is composed of random selections
+
+        # Player Straties ranked by score
+        pl_strat_ranking = self.pl_strat_sorted()
+
+        # Populate a new generation
+        next_gen_pl = []
+
+        for i in range(self.n_players // 3):
+            # Keep the top 33% player strategies from the rankings
+            next_gen_pl.append(pl_strat_ranking[i][0])
+
+            # Generate a random strategy, for variety in the next generation
+            rand_strat = self.game.random_strategy()
+            next_gen_pl.append(rand_strat)
+
+            # Create a mutant of the top 33% player strategies
+            mutant_strat = self.game.mutation(pl_strat_ranking[i][0])
+            next_gen_pl.append(mutant_strat)
+
+        self.player_strats = next_gen_pl        
 
 
 print("Input the Number of Soldiers")
 inp_n_soldiers = input("[User]: ")
 print("Input the Number of Battlefronts")
 inp_n_battlefronts = input("[User]: ")
-Blotto(inp_n_soldiers, inp_n_battlefronts)
+
+# Instantiate Game and Bot
+blotto_game = Blotto(inp_n_soldiers, inp_n_battlefronts)
+bot = Bot(blotto_game)
+
+# Run 1000 iterations
+for idx in range(1):
+
+    # Print generation number
+    print("Generation #{}".format(idx))
+
+    # Create new generation of strategies
+    bot.pl_strat_update()
+
+    # Print the best strategy of the bot for
+    # the current generation/iteration
+    curr_best_strat = bot.player_strats[0]
+    print("Current Best Strategy: {}".format(curr_best_strat))    
+    curr_strat_stats = blotto_game.pl_strategy_stats(curr_best_strat, blotto_game.r_dataset)
+    print("Current Best Strategy Performance Stats")
+    print("---------------------------------------")
+    print("Wins: {}".format(curr_strat_stats[0]))
+    print("Ties: {}".format(curr_strat_stats[1]))
+    print("Losses: {}".format(curr_strat_stats[2]))
+    print("Current Best Strategy Score: {}".format(blotto_game.calc_final_score(curr_strat_stats)))
+
+
+best_strat = bot.player_strats[0]
+print("Best Strategy: {}".format(best_strat))    
+strat_stats = blotto_game.pl_strategy_stats(best_strat, blotto_game.r_dataset)
+print("Best Strategy Performance Stats")
+print("---------------------------------------")
+print("Wins: {}".format(strat_stats[0]))
+print("Ties: {}".format(strat_stats[1]))
+print("Losses: {}".format(strat_stats[2]))
+print("Best Strategy Score: {}".format(blotto_game.calc_final_score(strat_stats)))
+
+
